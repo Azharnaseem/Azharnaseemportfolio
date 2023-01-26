750adbb69cb897b8436b61483c33e792bbb44df6
Azhar Naseem <94357283+Azharnaseem@users.noreply.github.com>
2023-01-27 00:07:48 +0500

    home and about part content chnage

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -4,19 +4,17 @@ import AvatarImg from "../../assets/images/avatar.jpg";
 
 const Avatar = () => {
   const classes = useStyles();
-  return (
-      <img src={AvatarImg} alt="Mehdi BHA" className={classes.avatarImg} />
-  );
+  return <img src={AvatarImg} alt="Azhar BHA" className={classes.avatarImg} />;
 };
 
 const useStyles = makeStyles((theme) => ({
   avatarImg: {
-    borderRadius:'50%',
-    width:"270px",
-    height:"270px",
-    objectFit:'cover',
-    objectPosition: "0 -20px",
-    boxShadow:theme.shadows[10]
+    borderRadius: "50%",
+    width: "270px",
+    height: "270px",
+    objectFit: "cover",
+    // objectPosition: "0 -20px",
+    boxShadow: theme.shadows[10],
   },
 }));
 
--- src/components/Navbar/Logo.js
+++ src/components/Navbar/Logo.js
@@ -1,30 +1,33 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Typography } from "@material-ui/core";
 import { Link } from "react-scroll";
 import Mehdibha from "../../assets/images/Mehdibha";
 
 const Logo = ({ setHomeIsActive, ...rest }) => {
-    const classes = useStyles();
-    return (
-        <Link
-            spy
-            smooth
-            duration={500}
-            offset={-70}
-            to="home"
-            onSetActive={() => setHomeIsActive(true)}
-            onSetInactive={() => setHomeIsActive(false)}
-            className={classes.root}
-        >
-            <Mehdibha {...rest} />
-        </Link>
-    );
+  const classes = useStyles();
+  return (
+    <Link
+      spy
+      smooth
+      duration={500}
+      offset={-70}
+      to="home"
+      onSetActive={() => setHomeIsActive(true)}
+      onSetInactive={() => setHomeIsActive(false)}
+      className={classes.root}
+    >
+      <Typography variant="body6" color="initial" component="h1">
+        Azhar Naseem
+      </Typography>
+      {/* <Mehdibha {...rest} /> */}
+    </Link>
+  );
 };
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        cursor: "pointer",
-    },
+  root: {
+    cursor: "pointer",
+  },
 }));
 
 export default Logo;
